6a533c14dcc267da37320dbf4bfe5f34107cc391
2014000029-FrancoPeruch <f.peruch.5ddb@gmail.com>
2026-04-27 13:51:14 -0600

    Add files via upload

---
+++ Proyecto_Guia4_FrancoPeruch/src/main/java/com/mycompany/proyecto_guia4_francoperuch/Proyecto_Guia4_FrancoPeruch.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_guia4_francoperuch;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Proyecto_Guia4_FrancoPeruch {
+
+    public static void main(String[] args) {
+        jfrmMenu v1 = new jfrmMenu();
+        v1.setVisible(true);
+    }
+}
